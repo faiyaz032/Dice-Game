58a22ec3d62663fc4812d8e4af90da7196e62cd3
Faiyaz Rahman <faiyazrahman03@gmail.com>
2020-12-23 23:27:48 +0600

    Selecting the elements

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 'use strict';
+//? Selecting the elements
+const elementScore0 = document.getElementById('score--0');
+const elementScore1 = document.getElementById('score--1');
+const elementCurrent0 = document.getElementById('current--0');
+const elementCurrent1 = document.getElementById('current--1');
+const roleDiceButton = document.querySelector('.btn--roll');
+const holdButton = document.querySelector('.btn--hold');
+const dice = document.querySelector('.dice');
+const newGameButton = document.querySelector('.btn--new');
