d2bbcd093c66435529631e309d9469dee85d5243
Faiyaz Rahman <faiyazrahman03@gmail.com>
2020-12-24 00:20:47 +0600

    Swticed to another player

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const elementScore0 = document.getElementById('score--0');
 const elementScore1 = document.getElementById('score--1');
 const elementCurrent0 = document.getElementById('current--0');
 const elementCurrent1 = document.getElementById('current--1');
+const elementPlayer0 = document.querySelector('.player--0');
+const elementPlayer1 = document.querySelector('.player--1');
 const roleDiceButton = document.querySelector('.btn--roll');
 const holdButton = document.querySelector('.btn--hold');
 const dice = document.querySelector('.dice');
@@ -13,7 +15,11 @@ const newGameButton = document.querySelector('.btn--new');
 elementScore0.textContent = 0;
 elementScore1.textContent = 0;
 dice.classList.add('hidden');
+
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
+
 //? Firing an event on Dice Button
 roleDiceButton.addEventListener('click', function () {
    //? generate Random Dice Number
@@ -25,10 +31,16 @@ roleDiceButton.addEventListener('click', function () {
 
    //? check for rolled 1: if true switch to next player
    //?check for role 1
-   if (!randomDiceNumber !== 1) {
-      //? and if its not one then the dice to to current score
+   if (randomDiceNumber !== 1) {
+      //? and if its not one then then keep rolling dice and add the score to current score
       currentScore += randomDiceNumber;
-      elementCurrent0.textContent = currentScore; //!We need to change it later
+      document.getElementById(`current--${activePlayer}`).textContent = currentScore;
    } else {
+      //? Switch to another player
+      document.getElementById(`current--${activePlayer}`).textContent = 0;
+      currentScore = 0;
+      activePlayer = activePlayer === 0 ? 1 : 0;
+      elementPlayer0.classList.toggle('player--active');
+      elementPlayer1.classList.toggle('player--active');
    }
 });
